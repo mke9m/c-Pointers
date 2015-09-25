07a42c510b529aa21a5d69e3f1b74009ae509a3d
ipv89 <michael.murray@hotmail.co.nz>
2015-09-25 13:40:22 +1200

    added delegates
    added delegates

--- cSharpDelegates/Program.cs
+++ cSharpDelegates/Program.cs
@@ -8,8 +8,16 @@ namespace cSharpDelegates
 {
     class Program
     {
+
+        public delegate int calculator(int value1, int value2);
         static void Main(string[] args)
         {
+            myUtil myUtilObject = new myUtil();
+
+            int i, j;
+            calculator addnumbers = new calculator(myUtilObject.addMethod);
+            calculator subnumbers = new calculator(myUtilObject.subMethod);
+
         }
     }
 }
---
+++ cSharpDelegates/myUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpDelegates
+{
+  public  class myUtil
+    {
+
+
+      public int addMethod(int num1, int num2)
+      {
+          return num1 + num2;
+
+
+      }
+
+      public int subMethod(int num1, int num2)
+      {
+          return num1 - num2;
+
+
+      }
+
+
+    }
+}
