ebd5e97cd51ab446c9da75868bc6c55c83fc9e66
ipv89 <michael.murray@hotmail.co.nz>
2015-09-25 13:59:07 +1200

    Completed Multi Cast example
    Completed Multi Cast example

---
+++ cSharpDelegates_MultiCast/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpDelegates_MultiCast
+{
+    class Program
+    {
+        static delegate void calculate(int value1, int value2);
+        static void Main(string[] args)
+        {
+            myUtil MyUtilObject = new myUtil();
+            int i, j;
+            calculate CalFunction = new calculate(MyUtilObject.addMethod);
+
+            CalFunction = CalFunction + new calculate(MyUtilObject.subMethod);
+            CalFunction = CalFunction + new calculate(MyUtilObject.divMethod);
+            CalFunction = CalFunction + new calculate(MyUtilObject.multiMethod);
+
+
+            Console.Write("enter a number:");
+            i = int.Parse(Console.ReadLine());
+
+            Console.Write("enter another number:");
+            j = int.Parse(Console.ReadLine());
+
+            CalFunction(i, j);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ cSharpDelegates_MultiCast/myUtil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpDelegates_MultiCast
+{
+    public class myUtil
+    {
+
+
+        public void addMethod(int num1, int num2)
+        {
+            Console.WriteLine("Adding Two Values : " + ( +num1 + num2));
+
+
+        }
+
+        public void subMethod(int num1, int num2)
+        {
+            Console.WriteLine("subtracting Two Values : " + ( +num1 - num2));
+
+
+        }
+
+        public void multiMethod(int num1, int num2)
+        {
+            Console.WriteLine("multiply Two Values : " + (+num1- num2));
+
+
+        }
+
+        public void divMethod(int num1, int num2)
+        {
+            Console.WriteLine("divide Two Values : " + (+num1 / num2));
+
+
+        }
+
+
+    }
+}
